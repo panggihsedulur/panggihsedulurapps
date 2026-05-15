4e5f0c54e837c7e291a9070585339b22ba5fb859
Adridinan Najmi Faza <104964855+najmifaza@users.noreply.github.com>
2026-05-15 20:58:14 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // Ensures Next dev (Turbopack) treats this folder as the root,
+    // even if there are other lockfiles in parent directories.
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
